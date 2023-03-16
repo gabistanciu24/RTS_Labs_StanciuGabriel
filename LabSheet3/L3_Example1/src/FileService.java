5eb19af505bc5655d4b7616d2dcf85f05be66931
Stanciu Gabriel <gabistanciu24@yahoo.com>
2023-03-16 14:26:17 +0200

    Add L3_Example3

--- LabSheet3/L3_Example1/src/FileService.java
+++ LabSheet3/L3_Example1/src/FileService.java
@@ -30,18 +30,22 @@ public class FileService {
         }
     }
 
-    public synchronized void write(String msg) {
-        Date date = new Date(System.currentTimeMillis());
-        out.println("Date: " + date);
-        out.println("Message: " + msg);
-        out.flush();
+    public void write(String msg) {
+        synchronized (this){
+            Date date = new Date(System.currentTimeMillis());
+            out.println("Date: " + date);
+            out.println("Message: " + msg);
+            out.flush();
+        }
     }
 
-    public synchronized String read() throws IOException {
-        String iterator, last = "no message to read";
-        while ((iterator = in.readLine()) != null) {
-            last = new Date(System.currentTimeMillis()) + " - " + iterator;
+    public String read() throws IOException {
+        synchronized (this) {
+            String iterator, last = "no message to read";
+            while ((iterator = in.readLine()) != null) {
+                last = new Date(System.currentTimeMillis()) + " - " + iterator;
+            }
+            return last;
         }
-        return last;
     }
 }
